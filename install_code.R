0ecf367952e402b80f355b486302ee237131800a
Anna Kozak <anna1993kozak@gmail.com>
2020-01-14 10:05:05 +0100

    add install code

---
+++ install_code.R
@@ -0,0 +1,13 @@
+install.packages(c("DALEX",
+                   "DALEXtra",
+                   "ingredients",
+                   "iBreakDown",
+                   "auditor",
+                   "vivo",
+                   "modelStudio",
+                   "mlr",
+                   "ranger",
+                   "e1071"))
+
+# On the top of that please install Anaconda Python distribution that wwill be necessary for integration practise.
+# Instruction can be seen here https://github.com/ModelOriented/DALEXtra#how-to-setup-anaconda
